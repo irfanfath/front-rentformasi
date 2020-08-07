03fa31f18e9cc7f0be09ec3955bffc57ae69aaa3
irfanfath <mirfanf7@gmail.com>
2020-08-08 03:24:34 +0700

    Delete Views Aapart & Hotel

--- src/Main.js
+++ src/Main.js
@@ -3,8 +3,6 @@ import { Route, HashRouter } from "react-router-dom";
 import DynamicScrollToTop from "./Component/DynamicScrollToTop";
 import Home from "./Container/Home/Home";
 import Header from "./Component/Navigation/Header";
-import Apartemen from "./Container/Produk/Apartemen";
-import Hotel from "./Container/Produk/Hotel";
 import Footer from "./Component/Navigation/Footer";
 import ListProduk from "./Container/Produk/ListProduk";
 
@@ -16,8 +14,6 @@ class Main extends Component {
                 <DynamicScrollToTop/>
                     <Route exact path="/" component={Home} />
                     <Route path="/listproduk/:idBarang" component={ListProduk}/>
-                    <Route path="/apartemen" component={Apartemen}/>
-                    <Route path="/hotel" component={Hotel}/>
                 <Footer/>
             </HashRouter>
         )
