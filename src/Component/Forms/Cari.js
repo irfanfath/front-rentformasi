fce62ac6a9ed7ece176a76507d19f62c1d99a495
irfanfath <mirfanf7@gmail.com>
2020-08-24 05:58:36 +0700

    Add Function Search Produk

--- src/Component/Forms/Cari.js
+++ src/Component/Forms/Cari.js
@@ -2,13 +2,21 @@ import React, { Component } from "react";
 import Kategori from "../Dropdown/Sorting/Kategori";
 
 class Cari extends Component{
+    state = {
+        cari: ""
+    }
+
+    handlecek = () => {
+        console.log(this.state.cari)
+    }
+    
     render(){
         return(
         <div className="margin-cari">
                 <div className="subscribe-cari">
                             <Kategori/>
-                            <input type="text" className="input-cari subscribe-input w-input" placeholder="Kata Kunci Pencarian" id="nama_barang1" />
-                            <i className="fa fa-search prefix"/> 
+                            <input type="text" className="input-cari subscribe-input w-input" placeholder="Kata Kunci Pencarian" id="nama_barang1" onChange={(e) => this.setState({cari: e.target.value})} />
+                            <i className="fa fa-search prefix" onClick={this.handlecek} /> 
                 </div>
         </div>
         )
